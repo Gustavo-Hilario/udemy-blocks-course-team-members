19744b965dea7ed514512c85ef756fa4a6c9a8ba
Gustavo_H <gustavo.hilario@automattic.com>
2023-07-24 17:55:41 -0300

    15. Configuring ESLint & Prettier for JavaScript Files

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { registerBlockType } from '@wordpress/blocks';
+import './style.scss';
+
+import Edit from './edit';
+import save from './save';
+import metadata from './block.json';
+
+const x = 0;
+/**
+ * Every block starts by registering a new block type definition.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
+ */
+registerBlockType( metadata.name, {
+	edit: Edit,
+	save,
+} );
