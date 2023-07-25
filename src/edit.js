bc42295dcf744e76db282c5f6141677306bcd612
Gustavo_H <gustavo.hilario@automattic.com>
2023-07-25 16:15:49 -0300

    16. Configuring Stylelint for Linting our SCSS Files

--- src/edit.js
+++ src/edit.js
@@ -1,13 +1,13 @@
-import { __ } from "@wordpress/i18n";
+import { __ } from '@wordpress/i18n';
 
-import { useBlockProps } from "@wordpress/block-editor";
+import { useBlockProps } from '@wordpress/block-editor';
 
-import "./editor.scss";
+import './editor.scss';
 
 export default function Edit() {
 	return (
-		<p {...useBlockProps()}>
-			{__("Boilerplate – hello from the editor!", "boilerplate")}
+		<p { ...useBlockProps() }>
+			{ __( 'Boilerplate – hello from the editor!', 'boilerplate' ) }
 		</p>
 	);
 }
