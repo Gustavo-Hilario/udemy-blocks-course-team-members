4a4a8103f7d59d612ad115d0820982145e161196
Gustavo_H <gustavo.hilario@automattic.com>
2023-09-03 12:42:47 -0500

    45. Creating the edit and save functions for the name and bio

--- src/team-member/index.js
+++ src/team-member/index.js
@@ -2,16 +2,31 @@ import { __ } from '@wordpress/i18n';
 
 import { registerBlockType } from '@wordpress/blocks';
 
+import Edit from './edit';
+import Save from './save';
+
 registerBlockType( 'blocks-course/team-member', {
 	// Similar to what's in the block.json file
 	title: __( 'Team Member', 'team-members' ),
 	description: __( 'Block showing a single team member', 'team-members' ),
 	icon: 'admin-users',
 	parent: [ 'blocks-course/team-members' ],
-	edit: () => {
-		return <div>Team member block</div>;
+	supports: {
+		// Removes support for an HTML mode
+		html: false,
 	},
-	save: () => {
-		return <div>Team member block</div>;
+	attributes: {
+		name: {
+			type: 'string',
+			source: 'html',
+			selector: 'h4',
+		},
+		bio: {
+			type: 'string',
+			source: 'html',
+			selector: 'p',
+		},
 	},
+	edit: Edit,
+	save: Save,
 } );
