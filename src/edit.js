25ebae749a4c9c4fd7087a910e386467c9ba3b2f
Gustavo_H <gustavo.hilario@automattic.com>
2023-09-04 19:16:49 -0500

    47. Adding an option for changing the number of columns

--- src/edit.js
+++ src/edit.js
@@ -1,10 +1,44 @@
-import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
+import {
+	useBlockProps,
+	InnerBlocks,
+	InspectorControls,
+} from '@wordpress/block-editor';
+
+import { PanelBody, RangeControl } from '@wordpress/components';
+
+import { __ } from '@wordpress/i18n';
+
+// import { check } from '@wordpress/icons';
 
 import './editor.scss';
 
-export default function Edit() {
+export default function Edit( { attributes, setAttributes } ) {
+	const { columns } = attributes;
+
+	const onChangeColumns = ( newColumnValue ) => {
+		setAttributes( { columns: newColumnValue } );
+	};
+
 	return (
-		<div { ...useBlockProps() }>
+		<div
+			{ ...useBlockProps( {
+				className: `has-${ columns }-columns`,
+			} ) }
+		>
+			<InspectorControls>
+				<PanelBody title={ __( 'Team Members' ) }>
+					<RangeControl
+						label={ __( 'Columns of Team Members' ) }
+						value={ columns }
+						min={ 1 }
+						max={ 6 }
+						step={ 1 }
+						onChange={ ( newColumnValue ) =>
+							onChangeColumns( newColumnValue )
+						}
+					></RangeControl>
+				</PanelBody>
+			</InspectorControls>
 			<InnerBlocks
 				allowedBlocks={ [ 'blocks-course/team-member' ] }
 				template={ [
